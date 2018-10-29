49604c29a8ccf91ad940b4b015c53d3108288be8
jjh56 <jjh56@DESKTOP-KQBELTQ>
2018-10-29 17:19:31 +0900

    엄마 똥꼬 추가

--- src/Holleworld.java
+++ src/Holleworld.java
@@ -3,5 +3,6 @@ public class Holleworld {
     public static void main(String[] args) {
     	System.out.println("Hello World");
     	System.out.println("hi five");
-    }
+    	System.out.println("엄마똥꼬바바보바보");
+    			  }
 }
